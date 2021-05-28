4afa5e5aca5902d4dfda141ad352584ede846278
Albert Gabrychowicz <albertg980@gmail.com>
2021-05-28 02:06:41 +0100

    Added tests for boundary values

--- Raindrops/RaindropsTests/UnitTests.cs
+++ Raindrops/RaindropsTests/UnitTests.cs
@@ -6,9 +6,15 @@ namespace RaindropsTests
     
     public class Tests
     {
+        // Given examples
         [TestCase(28, "Plong")]
         [TestCase(30, "PlingPlang")]
         [TestCase(34, "34")]
+
+        // Boundary values
+        [TestCase(Int32.MaxValue, "2147483647")]
+        [TestCase(Int32.MinValue, "-2147483648")]
+        [TestCase(0, "PlingPlangPlong")]
         public void InputANumber_ExpectedStringReturned (int n, string exp)
         {
             Assert.That(Raindrops.Raindrops.GetRaindrops(n), Is.EqualTo(exp));
