d01c56fe26a3cb4e7bdfa86891e920370a09dbe1
Albert Gabrychowicz <albertg980@gmail.com>
2021-05-28 02:04:43 +0100

    GetRaindrops method simplified

--- Raindrops/Raindrops/Raindrops.cs
+++ Raindrops/Raindrops/Raindrops.cs
@@ -10,8 +10,7 @@ namespace Raindrops
             if (n % 3 == 0) { result += "Pling"; }
             if (n % 5 == 0) { result += "Plang"; }
             if (n % 7 == 0) { result += "Plong"; }
-            if (result == "") { return n.ToString(); }
-            return result;
+            return result == "" ? n.ToString() : result;
         }
     }
 }
